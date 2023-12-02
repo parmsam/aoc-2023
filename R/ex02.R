c760b9e21256481d8fb7c5756bacf36776794f0d
parmsam <parmartsam@gmail.com>
2023-12-02 10:43:08 -0500

    add ex02 sltn

--- R/ex02.R
+++ R/ex02.R
@@ -9,7 +9,31 @@ library(tidyr)
 d1 <- adventdrob::advent_input(2, 2023)
 
 # Part 1 ------------------------------------------------------------------
+grab_max <- function(x) {
+    map_int(x, \(.x)max(as.numeric(str_extract(.x, "[0-9]+"))))
+}
 
+d1 %>%
+  separate_wider_regex(
+    x, patterns = c(
+      "Game ", Game = "[0-9]+", ":"
+  ), too_few = "align_start", cols_remove = F) %>%
+  mutate(reds = str_extract_all(x, "[0-9]+ red")) %>%
+  mutate(greens = str_extract_all(x, "[0-9]+ green")) %>%
+  mutate(blues = str_extract_all(x, "[0-9]+ blue")) %>% 
+  mutate(across(c(reds, greens, blues), grab_max)) %>%
+  filter(reds <= 12, greens <= 13, blues <= 14) %>%
+  summarize(tot = sum(as.numeric(Game)))
 
 # Part 2 ------------------------------------------------------------------
-
+d1 %>%
+  separate_wider_regex(
+    x, patterns = c(
+      "Game ", Game = "[0-9]+", ":"
+    ), too_few = "align_start", cols_remove = F) %>%
+  mutate(reds = str_extract_all(x, "[0-9]+ red")) %>%
+  mutate(greens = str_extract_all(x, "[0-9]+ green")) %>%
+  mutate(blues = str_extract_all(x, "[0-9]+ blue")) %>% 
+  mutate(across(c(reds, greens, blues), grab_max)) %>%
+  mutate(power = reds * greens * blues) %>%
+  summarize(tot = sum(as.numeric(power)))
